27552dd39472d61be3c42299321ea656fb571e65
Eric McCorkle <eric@shadowsun.net>
2013-01-15 00:36:58 -0500

    Actually added -Werror

--- IR/FlatIR/LLVMGen/VarAccess.hs
+++ IR/FlatIR/LLVMGen/VarAccess.hs
@@ -14,7 +14,7 @@
 -- along with this program; if not, write to the Free Software
 -- Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
 -- 02110-1301 USA
-{-# OPTIONS_GHC -funbox-strict-fields -Wall #-}
+{-# OPTIONS_GHC -funbox-strict-fields -Wall -Werror #-}
 
 -- | This module implements tools for compiling variable reads and
 -- writes.
