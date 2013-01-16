2fee0e87255e3831b312fc99d73b14ade2131b82
Eric McCorkle <eric@shadowsun.net>
2013-01-16 17:07:09 -0500

    Updated to use record-based FlatIR syntax

--- IR/FlatIR/LLVMGen/Types.hs
+++ IR/FlatIR/LLVMGen/Types.hs
@@ -33,7 +33,7 @@ import Foreign.Ptr
 import IR.FlatIR.Syntax
 import Prelude hiding (mapM_, mapM, foldr, foldl, sequence)
 
-import qualified IR.GC.Types as GC
+import qualified IR.Common.GC as GC
 import qualified LLVM.Core as LLVM
 
 -- | Generate an array mapping typenames to LLVM types.
@@ -54,54 +54,67 @@ genTypeDefs (Module { modTypes = types, modGCHeaders = gcheaders }) ctx =
         -- the map from typenames to (uninitialized) LLVM types to
         -- do this.
         genLLVMType :: Type -> IO LLVM.TypeRef
-        genLLVMType (FuncType retty argtys) =
+        genLLVMType (FuncType { funcTyRetTy = retty,
+                                funcTyArgTys = argtys }) =
           do
             retty' <- genLLVMType retty
             argtys' <- mapM genLLVMType argtys
             return (LLVM.functionType retty' argtys' False)
-        genLLVMType (StructType packed fields) =
+        genLLVMType (StructType { structPacked = packed,
+                                  structFields = fields }) =
           do
             fieldtys <- mapM (\(_, _, ty) -> genLLVMType ty) (elems fields)
             LLVM.structTypeInContext ctx fieldtys packed
-        genLLVMType (ArrayType (Just size) inner) =
+        genLLVMType (ArrayType { arrayLen = Just size,
+                                 arrayElemTy = inner }) =
           do
             inner' <- genLLVMType inner
             return (LLVM.arrayType inner' size)
-        genLLVMType (ArrayType Nothing inner) =
+        genLLVMType (ArrayType { arrayLen = Nothing,
+                                 arrayElemTy = inner }) =
           do
             inner' <- genLLVMType inner
             return (LLVM.arrayType inner' (0 :: Word))
-        genLLVMType (PtrType (GC.Native inner)) =
+        genLLVMType (PtrType { ptrElemTy = GC.Native inner }) =
           do
             inner' <- genLLVMType inner
             return (LLVM.pointerType inner' (0 :: Word))
-        genLLVMType (PtrType (GC.GC _ gcid)) =
+        genLLVMType (PtrType { ptrElemTy = GC.GC _ gcid }) =
           let
             (tname, _, _) = gcheaders ! gcid
           in do
             innerty' <- updateEntry tname
             return (LLVM.pointerType innerty' (0 :: Word))
-        genLLVMType (IdType tyid) = updateEntry tyid
-        genLLVMType (IntType _ 1) = LLVM.int1TypeInContext ctx
-        genLLVMType (IntType _ 8) = LLVM.int8TypeInContext ctx
-        genLLVMType (IntType _ 16) = LLVM.int16TypeInContext ctx
-        genLLVMType (IntType _ 32) = LLVM.int32TypeInContext ctx
-        genLLVMType (IntType _ 64) = LLVM.int64TypeInContext ctx
-        genLLVMType (IntType _ size) = LLVM.intTypeInContext ctx size
-        genLLVMType (FloatType 32) = LLVM.floatTypeInContext ctx
-        genLLVMType (FloatType 64) = LLVM.doubleTypeInContext ctx
-        genLLVMType (FloatType 128) = LLVM.fp128TypeInContext ctx
-        genLLVMType (FloatType n) =
+        genLLVMType (IdType { idName = tyid }) = updateEntry tyid
+        genLLVMType (IntType { intSize = 1 }) = LLVM.int1TypeInContext ctx
+        genLLVMType (IntType { intSize = 8 }) = LLVM.int8TypeInContext ctx
+        genLLVMType (IntType { intSize = 16 }) = LLVM.int16TypeInContext ctx
+        genLLVMType (IntType { intSize = 32 }) = LLVM.int32TypeInContext ctx
+        genLLVMType (IntType { intSize = 64 }) = LLVM.int64TypeInContext ctx
+        genLLVMType (IntType { intSize = size }) =
+          LLVM.intTypeInContext ctx size
+{-
+        genLLVMType (FloatType { floatSize = 16 }) =
+          LLVM.halfTypeInContext ctx
+-}
+        genLLVMType (FloatType { floatSize = 32 }) =
+          LLVM.floatTypeInContext ctx
+        genLLVMType (FloatType { floatSize = 64 }) =
+          LLVM.doubleTypeInContext ctx
+        genLLVMType (FloatType { floatSize = 128 }) =
+          LLVM.fp128TypeInContext ctx
+        genLLVMType (FloatType { floatSize = n }) =
           error ("Cannot generate floating point type with " ++
                  show n ++ " bits")
-        genLLVMType UnitType = error "Don't generate LLVM for UnitType"
+        genLLVMType (UnitType _) = error "Don't generate LLVM for UnitType"
 
         -- Grab the type entry for this type name, possibly
         -- (re)initializing it
         updateEntry :: Typename -> IO LLVM.TypeRef
         updateEntry ind =
           case types ! ind of
-            (_, Just (StructType packed fields)) ->
+            (_, Just (StructType { structPacked = packed,
+                                   structFields = fields })) ->
               do
                 ent <- readArray typemap ind
                 if LLVM.isOpaqueStruct ent
@@ -128,7 +141,7 @@ genTypeDefs (Module { modTypes = types, modGCHeaders = gcheaders }) ctx =
     -- everything else to null pointers.
     initEntry :: (String, Maybe Type) -> IO LLVM.TypeRef
     initEntry (str, Nothing) = LLVM.structCreateNamed ctx str
-    initEntry (str, Just (StructType _ _)) = LLVM.structCreateNamed ctx str
+    initEntry (str, Just (StructType {})) = LLVM.structCreateNamed ctx str
     initEntry _ = return nullPtr
   in do
     elems' <- mapM initEntry (elems types)
@@ -151,44 +164,56 @@ toLLVMType :: Graph gr =>
 toLLVMType (Module { modGCHeaders = gcheaders }) ctx types =
   let
     toLLVMType' :: Type -> IO LLVM.TypeRef
-    toLLVMType' (FuncType retty argtys) =
+    toLLVMType' (FuncType { funcTyRetTy = retty, funcTyArgTys = argtys }) =
       do
         retty' <- toLLVMType' retty
         argtys' <- mapM toLLVMType' argtys
         return (LLVM.functionType retty' argtys' False)
-    toLLVMType' (StructType packed fields) =
+    toLLVMType' (StructType { structPacked = packed,
+                              structFields = fields }) =
       do
         fieldtys <- mapM (\(_, _, ty) -> toLLVMType' ty) (elems fields)
         LLVM.structTypeInContext ctx fieldtys packed
-    toLLVMType' (ArrayType (Just size) inner) =
+    toLLVMType' (ArrayType { arrayLen = Just size,
+                             arrayElemTy = inner }) =
       do
         inner' <- toLLVMType' inner
         return (LLVM.arrayType inner' size)
-    toLLVMType' (ArrayType Nothing inner) =
+    toLLVMType' (ArrayType { arrayLen = Nothing,
+                             arrayElemTy = inner }) =
       do
         inner' <- toLLVMType' inner
         return (LLVM.arrayType inner' (0 :: Word))
-    toLLVMType' (PtrType (GC.Native inner)) =
+    toLLVMType' (PtrType { ptrElemTy = GC.Native inner }) =
        do
         inner' <- toLLVMType' inner
         return (LLVM.pointerType inner' (0 :: Word))
-    toLLVMType' (PtrType (GC.GC _ gcid)) =
+    toLLVMType' (PtrType { ptrElemTy = GC.GC _ gcid }) =
       let
         (tname, _, _) = gcheaders ! gcid
       in
         return (LLVM.pointerType (types ! tname) (0 :: Word))
-    toLLVMType' (IdType tyid) = return (types ! tyid)
-    toLLVMType' (IntType _ 1) = LLVM.int1TypeInContext ctx
-    toLLVMType' (IntType _ 8) = LLVM.int8TypeInContext ctx
-    toLLVMType' (IntType _ 16) = LLVM.int16TypeInContext ctx
-    toLLVMType' (IntType _ 32) = LLVM.int32TypeInContext ctx
-    toLLVMType' (IntType _ 64) = LLVM.int64TypeInContext ctx
-    toLLVMType' (IntType _ size) = LLVM.intTypeInContext ctx size
-    toLLVMType' (FloatType 32) = LLVM.floatTypeInContext ctx
-    toLLVMType' (FloatType 64) = LLVM.doubleTypeInContext ctx
-    toLLVMType' (FloatType 128) = LLVM.fp128TypeInContext ctx
-    toLLVMType' (FloatType n) =
-      error ("Cannot generate floating point type with " ++ show n ++ " bits")
-    toLLVMType' UnitType = error "Don't generate LLVM for UnitType"
+    toLLVMType' (IdType { idName = tyid }) = return (types ! tyid)
+    toLLVMType' (IntType { intSize = 1 }) = LLVM.int1TypeInContext ctx
+    toLLVMType' (IntType { intSize = 8 }) = LLVM.int8TypeInContext ctx
+    toLLVMType' (IntType { intSize = 16 }) = LLVM.int16TypeInContext ctx
+    toLLVMType' (IntType { intSize = 32 }) = LLVM.int32TypeInContext ctx
+    toLLVMType' (IntType { intSize = 64 }) = LLVM.int64TypeInContext ctx
+    toLLVMType' (IntType { intSize = size }) =
+      LLVM.intTypeInContext ctx size
+{-
+    toLLVMType' (FloatType { floatSize = 16 }) =
+      LLVM.halfTypeInContext ctx
+-}
+    toLLVMType' (FloatType { floatSize = 32 }) =
+      LLVM.floatTypeInContext ctx
+    toLLVMType' (FloatType { floatSize = 64 }) =
+      LLVM.doubleTypeInContext ctx
+    toLLVMType' (FloatType { floatSize = 126 }) =
+      LLVM.fp128TypeInContext ctx
+    toLLVMType' (FloatType { floatSize = n }) =
+      error ("Cannot generate floating point type with " ++
+             show n ++ " bits")
+    toLLVMType' (UnitType _) = error "Don't generate LLVM for UnitType"
   in
     toLLVMType'
