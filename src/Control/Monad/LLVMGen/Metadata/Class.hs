6d2cdf9ec626ca9a47dc3cc74be4df167642b65a
Eric McCorkle <emc2@metricspace.net>
2015-06-03 20:09:33 -0400

    Removed useless -funbox-strict-fields

--- src/Control/Monad/LLVMGen/Metadata/Class.hs
+++ src/Control/Monad/LLVMGen/Metadata/Class.hs
@@ -27,7 +27,7 @@
 -- OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 -- OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 -- SUCH DAMAGE.
-{-# OPTIONS_GHC -funbox-strict-fields -Wall -Werror #-}
+{-# OPTIONS_GHC -Wall -Werror #-}
 
 module Control.Monad.LLVMGen.Metadata.Class(
        MonadMetadata(..)
